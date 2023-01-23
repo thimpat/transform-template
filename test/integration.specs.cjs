ba1f1eaf009bdc0e6817ac44b9d9d326c4c242f8
Patrice THIMOTHEE <thimpat@gmail.com>
2023-01-23 02:03:03 +0000

    TEST: Add test

--- test/integration.specs.cjs
+++ test/integration.specs.cjs
@@ -22,7 +22,15 @@ describe('The CLI converter', function ()
         rmSync("out2", {recursive: true, force: true});
     })
 
-    it('should convert the directory', function ()
+    it('should convert a file', function ()
+    {
+        shelljs.exec(
+            `node ../cli.cjs --source ../demo/file3.js --datafile ../package.json --outputfile ./out2/toto.cjs`
+        );
+        expect("./out2/toto.cjs").to.be.a.file().with.contents.that.match(/const version = "##aaaa##.transform-template";/);
+    });
+
+    it('should convert the demo directory', function ()
     {
         shelljs.exec("node ../cli.cjs --source ../demo --output ./out2 --datafile ../package.json");
         expect("./out2/file3.js").to.be.a.file().with.contents.that.match(/const version = "##aaaa##.transform-template";/);
